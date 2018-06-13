08a35dae22646cead4534d5812c06d1ccc60e8ba
MayaCoro <mayascoro@gmail.com>
2018-06-13 23:02:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello World!"
+  end
+  
+  get '/name'do
+    "My name is Maya"
+  end
+  
+  get '/hometown'do
+    "My hometown is Phoenix"
+  end
+  
+  get '/favorite-song'do
+    "My favorite song is 1999 by Prince"
+  end
+  
 end
